09eab8aac17386cbc1f6c122bff45e9f1a56e0a3
Ryan  Yim <ryanchichun.yim23@ycdsbk12.ca>
2020-12-07 18:55:33 +0000

    Completed 1 and 2

--- problem1.py
+++ problem1.py
@@ -1,1 +1,19 @@
+'''
+-------------------------------------------------------------------------------
+Name:		problem1
+Purpose:	To distribute the amount of chicken to students evenly and to give Mr. Fabroa the rest of the chicken
 
+Author:	Yim.R
+
+Created: 07/12/2020
+------------------------------------------------------------------------------
+'''
+
+#input the celsius 
+celsius = float(input("Enter the degrees celsius:"))
+
+# Use the celsius to fahrenheit
+fahrenheit = (celsius*(9/5)) + 32
+
+#Should output the degrees fahrenheit
+print ("This is your degrees fahrenheit:" + str(fahrenheit))
